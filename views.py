c6aa561d6b99fb4aeb913d52c4bc37e0fb6030b0
Marcus Furlong <furlongm@vpac.org>
2011-07-22 17:07:58 +1000

    additional dashboard widget

--- views.py
+++ views.py
@@ -13,6 +13,7 @@ from datetime import datetime, date, time, timedelta
 from patchman.hosts.models import Host
 from patchman.operatingsystems.models import OS, OSGroup
 from patchman.repos.models import Repository
+from patchman.packages.models import Package
 
 @login_required
 def dashboard(request):
@@ -23,17 +24,20 @@ def dashboard(request):
         site = {'name':'', 'domainname':''}
 
     stale_hosts = Host.objects.filter(lastreport__lt = (datetime.now() + timedelta(-14)))
+    stale_repos = Repository.objects.filter(timestamp__lt = (datetime.now() + timedelta(-14)))
     norepo_hosts = Host.objects.filter(repos__isnull = True, os__osgroup__repos__isnull = True)
     norepo_osgroups = OSGroup.objects.filter(repos__isnull = True)
+    norepo_packages = Package.objects.filter(repository__isnull = True)
     lonely_oses = OS.objects.filter(osgroup__isnull = True)
     failed_repos = Repository.objects.filter(last_access_ok = False)
     secupdate_hosts = Host.objects.filter(updates__security = True, updates__isnull = False).values('hostname').annotate(Count('hostname'))
     update_hosts = Host.objects.filter(updates__security = False, updates__isnull = False).values('hostname').annotate(Count('hostname'))
-    unused_repos = Repository.objects.filter(host__isnull = True, osgroup__isnull = True) 
+    unused_repos = Repository.objects.filter(host__isnull = True, osgroup__isnull = True)
 
     return render_to_response('dashboard/index.html',
         {'lonely_oses': lonely_oses, 'norepo_hosts': norepo_hosts,
-        'stale_hosts': stale_hosts, 'site' : site, 
+        'stale_hosts': stale_hosts, 'stale_repos': stale_repos,
+        'site' : site, 'norepo_packages' : norepo_packages, 
         'secupdate_hosts' : secupdate_hosts, 'update_hosts' : update_hosts,
         'norepo_osgroups' : norepo_osgroups, 'unused_repos': unused_repos,
         'failed_repos' : failed_repos},
