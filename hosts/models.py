98ca0879228ca45a04f8e153929e0ba36a35e3b1
Marcus Furlong <furlongm@vpac.org>
2011-10-07 13:40:30 +1100

    add gpl v3 license

--- hosts/models.py
+++ hosts/models.py
@@ -1,3 +1,19 @@
+# Copyright 2011 VPAC <furlongm@vpac.org>
+#
+# This file is part of Patchman.
+#
+# Patchman is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, version 3 only.
+#
+# Patchman is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Patchman. If not, see <http://www.gnu.org/licenses/>
+
 from django.db import models
 from django.db.models import Q, Count
 from django.dispatch import Signal
@@ -27,6 +43,9 @@ class Host(models.Model):
     repos = models.ManyToManyField(Repository)
     updates = models.ManyToManyField(PackageUpdate)
     reboot_required = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ('hostname',)
 
     def __unicode__(self):
         return self.hostname
