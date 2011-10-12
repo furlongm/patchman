1c319d09cf8523f80c29239212e41edaac769564
Marcus Furlong <furlongm@vpac.org>
2011-10-13 09:57:57 +1100

    Added initial south migrations

---
+++ patchman/reports/migrations/0002_auto__add_field_report_repos.py
@@ -0,0 +1,41 @@
+# encoding: utf-8
+import datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+class Migration(SchemaMigration):
+    
+    def forwards(self, orm):
+        
+        # Adding field 'Report.repos'
+        db.add_column('reports_report', 'repos', self.gf('django.db.models.fields.TextField')(null=True, blank=True), keep_default=False)
+    
+    
+    def backwards(self, orm):
+        
+        # Deleting field 'Report.repos'
+        db.delete_column('reports_report', 'repos')
+    
+    
+    models = {
+        'reports.report': {
+            'Meta': {'object_name': 'Report'},
+            'arch': ('django.db.models.fields.CharField', [], {'max_length': '255', 'null': 'True'}),
+            'domain': ('django.db.models.fields.CharField', [], {'max_length': '255', 'null': 'True'}),
+            'host': ('django.db.models.fields.CharField', [], {'max_length': '255', 'null': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'kernel': ('django.db.models.fields.CharField', [], {'max_length': '255', 'null': 'True'}),
+            'os': ('django.db.models.fields.CharField', [], {'max_length': '255', 'null': 'True'}),
+            'packages': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'}),
+            'processed': ('django.db.models.fields.BooleanField', [], {'default': 'False', 'blank': 'True'}),
+            'protocol': ('django.db.models.fields.CharField', [], {'max_length': '255', 'null': 'True'}),
+            'report_ip': ('django.db.models.fields.IPAddressField', [], {'max_length': '15', 'null': 'True'}),
+            'repos': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'}),
+            'tags': ('django.db.models.fields.CharField', [], {'default': "''", 'max_length': '255', 'null': 'True'}),
+            'time': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            'useragent': ('django.db.models.fields.CharField', [], {'max_length': '255', 'null': 'True'})
+        }
+    }
+    
+    complete_apps = ['reports']
---
+++ patchman/repos/migrations/0002_auto__add_unique_repository_url.py
@@ -0,0 +1,72 @@
+# encoding: utf-8
+import datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+class Migration(SchemaMigration):
+    
+    def forwards(self, orm):
+        
+        # Adding unique constraint on 'Repository', fields ['url']
+        db.create_unique('repos_repository', ['url'])
+    
+    
+    def backwards(self, orm):
+        
+        # Removing unique constraint on 'Repository', fields ['url']
+        db.delete_unique('repos_repository', ['url'])
+    
+    
+    models = {
+        'arch.machinearchitecture': {
+            'Meta': {'object_name': 'MachineArchitecture'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '255'})
+        },
+        'arch.packagearchitecture': {
+            'Meta': {'object_name': 'PackageArchitecture'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '255'})
+        },
+        'packages.package': {
+            'Meta': {'object_name': 'Package'},
+            'arch': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['arch.PackageArchitecture']"}),
+            'description': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'}),
+            'epoch': ('django.db.models.fields.CharField', [], {'max_length': '255', 'null': 'True', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['packages.PackageName']"}),
+            'packagetype': ('django.db.models.fields.CharField', [], {'max_length': '1', 'null': 'True', 'blank': 'True'}),
+            'release': ('django.db.models.fields.CharField', [], {'max_length': '255', 'null': 'True', 'blank': 'True'}),
+            'url': ('django.db.models.fields.URLField', [], {'max_length': '255', 'null': 'True', 'blank': 'True'}),
+            'version': ('django.db.models.fields.CharField', [], {'max_length': '255'})
+        },
+        'packages.packagename': {
+            'Meta': {'object_name': 'PackageName'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '255'})
+        },
+        'repos.repopackage': {
+            'Meta': {'object_name': 'RepoPackage'},
+            'enabled': ('django.db.models.fields.BooleanField', [], {'default': 'True', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'package': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['packages.Package']"}),
+            'repo': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['repos.Repository']"})
+        },
+        'repos.repository': {
+            'Meta': {'object_name': 'Repository'},
+            'arch': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['arch.MachineArchitecture']"}),
+            'enabled': ('django.db.models.fields.BooleanField', [], {'default': 'False', 'blank': 'True'}),
+            'file_checksum': ('django.db.models.fields.CharField', [], {'max_length': '255', 'null': 'True', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'last_access_ok': ('django.db.models.fields.BooleanField', [], {'default': 'False', 'blank': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '255'}),
+            'packages': ('django.db.models.fields.related.ManyToManyField', [], {'symmetrical': 'False', 'to': "orm['packages.Package']", 'null': 'True', 'through': "orm['repos.RepoPackage']", 'blank': 'True'}),
+            'repotype': ('django.db.models.fields.CharField', [], {'max_length': '1'}),
+            'security': ('django.db.models.fields.BooleanField', [], {'default': 'False', 'blank': 'True'}),
+            'timestamp': ('django.db.models.fields.DateTimeField', [], {}),
+            'url': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '255'})
+        }
+    }
+    
+    complete_apps = ['repos']
