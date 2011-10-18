464cec9039b9e81024796625e1544cc333c60867
Sam Morrison <sam@vpac.org>
2011-10-18 13:18:21 +1100

    Removed comments for admin site

--- patchman/urls.py
+++ patchman/urls.py
@@ -16,9 +16,8 @@
 
 from django.conf.urls.defaults import *
 from django.conf import settings
-
-# Uncomment the next two lines to enable the admin:
 from django.contrib import admin
+
 admin.autodiscover()
 
 urlpatterns = patterns('',
@@ -35,7 +34,6 @@ urlpatterns = patterns('',
     # Uncomment the admin/doc line below to enable admin documentation:
     #(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
-    # Uncomment the next line to enable the admin:
     (r'^admin/', include(admin.site.urls)),
 )
 
