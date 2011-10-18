11a6b6f28949a0a89130fa8ab5e2914cadd8f2f6
Marcus Furlong <furlongm@vpac.org>
2011-10-18 18:27:51 +1100

    fix some brokenness, move non-model stuff back to cli tool

--- patchman/reports/signals.py
+++ patchman/reports/signals.py
@@ -1,4 +1,6 @@
 from django.dispatch import Signal
 
 report_processed = Signal(providing_args=["report"])
+package_processed = Signal(providing_args=["package"])
+repo_processed = Signal(providing_args=["repo"])
 
