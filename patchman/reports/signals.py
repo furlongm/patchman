532b9c06a9d69acdf9d7bd80b4ce66518b59fa83
Marcus Furlong <furlongm@vpac.org>
2011-10-18 23:04:44 +1100

    add pbar index updating for repos

--- patchman/reports/signals.py
+++ patchman/reports/signals.py
@@ -3,6 +3,6 @@ from django.dispatch import Signal
 report_processed = Signal(providing_args=["report"])
 numpackages = Signal(providing_args=["host", "numpackages"])
 numrepos = Signal(providing_args=["host", "numrepos"])
-package_processed = Signal(providing_args=["package"])
-repo_processed = Signal(providing_args=["repo"])
+package_processed = Signal(providing_args=["index"])
+repo_processed = Signal(providing_args=["index"])
 
