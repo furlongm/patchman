4c6be8c8c9d2b8496f6f883eb189a955e4f7c431
Marcus Furlong <furlongm@vpac.org>
2011-10-18 22:46:29 +1100

    add report signals to set up repos pbar

--- patchman/reports/signals.py
+++ patchman/reports/signals.py
@@ -1,6 +1,8 @@
 from django.dispatch import Signal
 
 report_processed = Signal(providing_args=["report"])
+numpackages = Signal(providing_args=["host", "numpackages"])
+numrepos = Signal(providing_args=["host", "numrepos"])
 package_processed = Signal(providing_args=["package"])
 repo_processed = Signal(providing_args=["repo"])
 
