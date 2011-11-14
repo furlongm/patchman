2280251b3ca1634ed8326663657aee5ecd387143
Marcus Furlong <furlongm@vpac.org>
2011-11-14 14:30:37 +1100

    add reverse support

--- patchman/reports/views.py
+++ patchman/reports/views.py
@@ -23,6 +23,7 @@ from django.template import RequestContext
 from django.http import HttpResponseRedirect
 from django.http import Http404
 from django.core.paginator import Paginator, InvalidPage, EmptyPage
+from django.core.urlresolvers import reverse
 from django.db.models import Q, Count
 from django.conf import settings
 from django.contrib import messages
