43a7f3652453cb7652d76651cf9754ca61f1efea
Marcus Furlong <furlongm@vpac.org>
2011-11-23 15:22:18 +1100

    pep8 stuff for tests

--- patchman/repos/tests.py
+++ patchman/repos/tests.py
@@ -23,6 +23,7 @@ Replace these with more appropriate tests for your application.
 
 from django.test import TestCase
 
+
 class SimpleTest(TestCase):
     def test_basic_addition(self):
         """
@@ -36,4 +37,3 @@ Another way to test that 1 + 1 is equal to 2.
 >>> 1 + 1 == 2
 True
 """}
-
