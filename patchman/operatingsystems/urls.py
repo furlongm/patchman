a1926d2ba60a7c06b5b6d11813f9495534660ad8
Marcus Furlong <furlongm@vpac.org>
2012-03-07 17:23:45 +1100

    add os group delete option, fix css error

--- patchman/operatingsystems/urls.py
+++ patchman/operatingsystems/urls.py
@@ -22,4 +22,5 @@ urlpatterns = patterns('patchman.operatingsystems.views',
     url(r'^groups/$', 'osgroup_list', name='osgroup_list'),
     url(r'^(?P<os_id>[-.\w]+)/$', 'os_detail', name='os_detail'),
     url(r'^groups/(?P<osgroup_id>[-.\w]+)/$', 'osgroup_detail', name='osgroup_detail'),
+    url(r'^groups/(?P<osgroup_id>[-.\w]+)/delete/$', 'osgroup_delete', name='osgroup_delete'),
 )
