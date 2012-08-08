55fefb149df2e7d2737f4027f7b3b8876094f5a5
Marcus Furlong <furlongm@vpac.org>
2012-08-08 17:52:18 +1000

    updated conf/settings.py for django 1.4

--- patchman/conf/settings.py
+++ patchman/conf/settings.py
@@ -32,17 +32,15 @@ USE_I18N = True
 # calendars according to the current locale
 USE_L10N = True
 
-# URL that handles the media served from MEDIA_ROOT. Make sure to use a
-# trailing slash if there is a path component (optional in other cases).
-# Examples: "http://media.lawrence.com", "http://example.com/media/"
-MEDIA_URL = '/patchman_media/'
+# URL prefix for static files.
+# Example: "http://media.lawrence.com/static/"
+STATIC_URL = '/patchman_media/'
 
-MEDIA_ROOT = '/usr/share/patchman/media'
-
-# URL prefix for admin media -- CSS, JavaScript and images. Make sure to use a
-# trailing slash.
-# Examples: "http://foo.com/media/", "/media/".
-ADMIN_MEDIA_PREFIX = '/django_media/'
+# Absolute path to the directory static files should be collected to.
+# Don't put anything in this directory yourself; store your static files
+# in apps' "static/" subdirectories and in STATICFILES_DIRS.
+# Example: "/home/media/media.lawrence.com/static/"
+STATIC_ROOT = '/home/furlong/src/patchman/media/'
 
 # List of callables that know how to import templates from various sources.
 TEMPLATE_LOADERS = (
@@ -62,9 +60,8 @@ MIDDLEWARE_CLASSES = (
 ROOT_URLCONF = 'patchman.urls'
 
 TEMPLATE_CONTEXT_PROCESSORS = (
-# django 1.4:
-#    'django.contrib.auth.context_processors.auth',
-    'django.core.context_processors.auth',
+    'django.contrib.auth.context_processors.auth',
+    'django.core.context_processors.static',
     'django.core.context_processors.debug',
     'django.core.context_processors.media',
     'django.core.context_processors.request',
@@ -85,6 +82,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.admin',
     'django.contrib.admindocs',
+    'django.contrib.staticfiles',
     'django_extensions',
     'andsome',
     'south',
