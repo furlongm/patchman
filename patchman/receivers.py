2d974edc08361b058817d96327b71d47bf6a0638
Marcus Furlong <furlongm@gmail.com>
2014-02-12 17:50:51 +1100

    fix verbosity when using cli

--- patchman/receivers.py
+++ patchman/receivers.py
@@ -15,18 +15,20 @@
 # along with Patchman. If not, see <http://www.gnu.org/licenses/>
 
 import os
-import sys
 
 from django.dispatch import receiver
 
-from util import create_pbar, update_pbar
-from signals import progress_info_s, progress_update_s, info_message, error_message, debug_message
+from util import create_pbar, update_pbar, print_nocr, get_verbosity
+from signals import progress_info_s, progress_update_s, \
+    info_message, error_message, debug_message
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "patchman.settings")
 from django.conf import settings
 
+
 @receiver(progress_info_s)
 def progress_info_r(**kwargs):
+
     ptext = kwargs.get('ptext')
     plen = kwargs.get('plen')
     if ptext and plen:
@@ -35,6 +37,7 @@ def progress_info_r(**kwargs):
 
 @receiver(progress_update_s)
 def progress_update_r(**kwargs):
+
     index = kwargs.get('index')
     if index:
         update_pbar(index)
@@ -42,23 +45,23 @@ def progress_update_r(**kwargs):
 
 @receiver(info_message)
 def print_info_message(sender=None, **kwargs):
+
     text = kwargs.get('text')
-    if settings.VERBOSE:
-        print text,
-        sys.stdout.softspace = False
+    if get_verbosity():
+        print_nocr(text)
 
 
 @receiver(error_message)
 def print_error_message(sender, **kwargs):
+
     text = kwargs.get('text')
     if text:
-        print text,
-        sys.stdout.softspace = False
+        print_nocr(text)
 
 
 @receiver(debug_message)
 def print_debug_message(sender, **kwargs):
+
     text = kwargs.get('text')
-    if settings.DEBUG and text:
-        print text,
-        sys.stdout.softspace = False
+    if get_verbosity() and settings.DEBUG and text:
+        print_nocr(text)
