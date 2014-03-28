0e070980a798d8f67f4f023f8e306a9817d0f896
Marcus Furlong <furlongm@gmail.com>
2014-03-28 23:50:58 +1100

    add project path to sys.path for wsgi

--- patchman/wsgi.py
+++ patchman/wsgi.py
@@ -7,6 +7,9 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
 """
 import os
+import sys
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "../")))
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "patchman.settings")
 
