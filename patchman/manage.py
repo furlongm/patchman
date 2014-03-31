0e23e5b0ab3a139dec21b94668d427bc7131381f
Marcus Furlong <furlongm@gmail.com>
2014-03-31 22:54:58 +1100

    minor fixes and updates

--- patchman/manage.py
+++ patchman/manage.py
@@ -5,6 +5,8 @@ import sys
 
 if __name__ == "__main__":
 
+    sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "../")))
+
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "patchman.settings")
 
     from django.core.management import execute_from_command_line
--- patchman/settings.py
+++ patchman/settings.py
@@ -2,7 +2,7 @@
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
-BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 PROJECT_DIR = BASE_DIR
 
 DEBUG = False
