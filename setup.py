8d87f85ff7a4d3fa3c75b78f0c41d6c210d899bc
Marcus Furlong <furlongm@gmail.com>
2015-02-04 04:30:57 +0000

    switch to setuptools for setup.py

--- setup.py
+++ setup.py
@@ -1,29 +1,9 @@
-from distutils.core import setup
-from distutils.command.install_data import install_data
-from distutils.command.install import INSTALL_SCHEMES
+from setuptools import setup
 import os
-import sys
 
 with open('VERSION.txt', 'r') as f:
     version = f.readline().strip()
 
-class osx_install_data(install_data):
-    # On MacOS, the platform-specific lib dir is /System/Library/Framework/Python/.../
-    # which is wrong. Python 2.5 supplied with MacOS 10.5 has an Apple-specific fix
-    # for this in distutils.command.install_data#306. It fixes install_lib but not
-    # install_data, which is why we roll our own install_data class.
-
-    def finalize_options(self):
-        # By the time finalize_options is called, install.install_lib is set to the
-        # fixed directory, so we set the installdir to install_lib. The
-        # install_data class uses ('install_data', 'install_dir') instead.
-        self.set_undefined_options('install', ('install_lib', 'install_dir'))
-        install_data.finalize_options(self)
-
-if sys.platform == "darwin":
-    cmdclasses = {'install_data': osx_install_data}
-else:
-    cmdclasses = {'install_data': install_data}
 
 def fullsplit(path, result=None):
     """
@@ -39,41 +19,25 @@ def fullsplit(path, result=None):
         return result
     return fullsplit(head, [tail] + result)
 
-# Tell distutils to put the data_files in platform-specific installation
-# locations. See here for an explanation:
-# http://groups.google.com/group/comp.lang.python/browse_thread/thread/35ec7b2fed36eaec/2105ee4d9e8042cb
-for scheme in INSTALL_SCHEMES.values():
-    scheme['data'] = scheme['purelib']
+packages = []
+for dirpath, dirnames, filenames in os.walk('patchman'):
+    # Ignore dirnames that start with '.'
+    for i, dirname in enumerate(dirnames):
+        if dirname.startswith('.'):
+            del dirnames[i]
+    if filenames:
+        packages.append('.'.join(fullsplit(dirpath)))
 
-# Compile the list of packages available, because distutils doesn't have
-# an easy way to do this.
-scripts, packages, data_files = [], [], []
-root_dir = os.path.dirname(__file__)
-if root_dir != '':
-    os.chdir(root_dir)
-code_dir = 'patchman'
-
-for dirpath, dirnames, filenames in os.walk(code_dir):
+data_files = [
+              ('/etc/patchman', ['etc/patchman-apache.conf']),
+              ('/etc/patchman', ['client/patchman-client.conf'])
+]
+for dirpath, dirnames, filenames in os.walk('etc'):
     # Ignore dirnames that start with '.'
     for i, dirname in enumerate(dirnames):
         if dirname.startswith('.'): del dirnames[i]
-    if '__init__.py' in filenames:
-        packages.append('.'.join(fullsplit(dirpath)))
-    elif filenames:
-        data_files.append([dirpath, [os.path.join(dirpath, f) for f in filenames]])
-
-# Small hack for working with bdist_wininst.
-# See http://mail.python.org/pipermail/distutils-sig/2004-August/004134.html
-if len(sys.argv) > 1 and sys.argv[1] == 'bdist_wininst':
-    for file_info in data_files:
-        file_info[0] = '\\PURELIB\\%s' % file_info[0]
-
-for dirpath, dirnames, filenames in os.walk('etc'):
-    for i, dirname in enumerate(dirnames):
-        if dirname.startswith('.'): del dirnames[i]
     if filenames:
-        data_files.append([dirpath.replace('etc', '/etc/patchman'), [os.path.join(dirpath, f) for f in filenames]])
-
+       data_files.append([dirpath.replace('etc', '/etc/patchman'), [os.path.join(dirpath, f) for f in filenames]])
 
 for dirpath, dirnames, filenames in os.walk('media'):
     # Ignore dirnames that start with '.'
@@ -89,16 +53,6 @@ for dirpath, dirnames, filenames in os.walk('templates'):
     if filenames:
         data_files.append([dirpath.replace('templates', '/usr/share/patchman/templates'), [os.path.join(dirpath, f) for f in filenames]])
 
-data_files.append(
-    ('/etc/patchman', ['etc/patchman-apache.conf']),
-)
-scripts.append(
-    ('sbin/patchman'),
-)
-scripts.append(
-    ('sbin/patchman-set-secret-key'),
-)
-
 setup(
     name = 'patchman',
     version = version,
@@ -107,17 +61,6 @@ setup(
     author_email = 'furlongm@gmail.com',
     description = 'Patchman is a django-based patch status monitoring tool for linux systems.',
     packages = packages,
-    cmdclass = cmdclasses,
     data_files = data_files,
-    scripts = scripts,
-)
-
-setup(
-    name = 'patchman-client',
-    version = version,
-    url = 'https://www.github.com/furlongm/patchman/',
-    author = 'Marcus Furlong',
-    author_email = 'furlongm@gmail.com',
-    description = 'Patchman is a django-based patch status monitoring tool for linux systems.',
-    scripts = ['client/patchman-client'],
+    scripts = ['sbin/patchman', 'sbin/patchman-set-secret-key'],
 )
