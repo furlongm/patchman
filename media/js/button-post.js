1d83d6fc1d2512ca905e327c5615748528a1842e
Marcus Furlong <furlongm@gmail.com>
2016-06-01 19:52:05 +1000

    fix some js and css nits

--- media/js/button-post.js
+++ media/js/button-post.js
@@ -2,10 +2,11 @@ function repo_toggle_enabled(id, element, e) {
     e.preventDefault();
     var url = id + "toggle_enabled/";
     $.post(url);
-    if (element.innerHTML.indexOf("icon-no.gif") > -1)
-        var newHTML = element.innerHTML.replace("icon-no.gif", "icon-yes.gif").replace('Disabled', 'Enabled');
-    else
-        var newHTML = element.innerHTML.replace("icon-yes.gif", "icon-no.gif").replace('Enabled', 'Disabled');
+    if (element.innerHTML.indexOf("icon-no.gif") > -1) {
+        var newHTML = element.innerHTML.replace("icon-no.gif", "icon-yes.gif").replace("Disabled", "Enabled");
+    } else {
+        var newHTML = element.innerHTML.replace("icon-yes.gif", "icon-no.gif").replace("Enabled", "Disabled");
+    }
     element.innerHTML = newHTML;
 }
 
@@ -13,9 +14,10 @@ function repo_toggle_security(id, element, e) {
     e.preventDefault();
     var url = id + "toggle_security/";
     $.post(url);
-    if (element.innerHTML.indexOf("icon-no.gif") > -1)
-        var newHTML = element.innerHTML.replace("icon-no.gif", "icon-yes.gif").replace('Non-Security', 'Security');
-    else
-        var newHTML = element.innerHTML.replace("icon-yes.gif", "icon-no.gif").replace('Security', 'Non-Security');
+    if (element.innerHTML.indexOf("icon-no.gif") > -1) {
+        var newHTML = element.innerHTML.replace("icon-no.gif", "icon-yes.gif").replace("Non-Security", "Security");
+    } else {
+        var newHTML = element.innerHTML.replace("icon-yes.gif", "icon-no.gif").replace("Security", "Non-Security");
+    }
     element.innerHTML = newHTML;
 }
