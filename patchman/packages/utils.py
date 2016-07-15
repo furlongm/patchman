2cbfc9671ac83bbd7acc25bcf6acded763a1742e
Marcus Furlong <furlongm@gmail.com>
2016-07-15 18:54:02 +1000

    use get_or_create_package as a reusable function

--- patchman/packages/utils.py
+++ patchman/packages/utils.py
@@ -15,6 +15,9 @@
 # You should have received a copy of the GNU General Public License
 # along with Patchman. If not, see <http://www.gnu.org/licenses/>
 
+from django.db import IntegrityError, DatabaseError, transaction
+from patchman.packages.models import PackageName, Package
+from patchman.arch.models import PackageArchitecture
 
 
 def find_evr(s):
@@ -60,3 +63,47 @@ def find_version(s, epoch, release):
     except ValueError:
         r = len(s)
     return s[e:r]
+
+
+def get_or_create_package(name, epoch, version, release, arch, p_type):
+    """ Get or create a Package object. Returns the object or None
+    """
+    package = None
+    name = name.lower()
+    if name == 'gpg-pubkey':
+        return
+
+    try:
+        with transaction.atomic():
+            package_names = PackageName.objects.all()
+            p_name, c = package_names.get_or_create(name=name)
+    except IntegrityError as e:
+        error_message.send(sender=None, text=e)
+        p_name = package_names.get(name=name)
+    except DatabaseError as e:
+        error_message.send(sender=None, text=e)
+
+    package_arches = PackageArchitecture.objects.all()
+    with transaction.atomic():
+        p_arch, c = package_arches.get_or_create(name=arch)
+
+    try:
+        with transaction.atomic():
+            packages = Package.objects.all()
+            package, c = packages.get_or_create(name=p_name,
+                                                arch=p_arch,
+                                                epoch=epoch,
+                                                version=version,
+                                                release=release,
+                                                packagetype=p_type)
+    except IntegrityError as e:
+        error_message.send(sender=None, text=e)
+        package = packages.get(name=p_name,
+                               arch=p_arch,
+                               epoch=epoch,
+                               version=version,
+                               release=release,
+                               packagetype=p_type)
+    except DatabaseError as e:
+        error_message.send(sender=None, text=e)
+    return package
