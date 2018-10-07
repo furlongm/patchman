db05a9ef5fa78904ad0c89944fe37603401947cf
Marcus Furlong <furlongm@gmail.com>
2018-10-07 00:26:06 -0400

    easier defaults for settings

--- etc/local_settings.py
+++ etc/local_settings.py
@@ -3,29 +3,18 @@
 DEBUG = False
 
 ADMINS = (
-#    ('Your Name', 'you@example.com'),
+    ('Your Name', 'you@example.com'),
 )
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',  # Add 'postgresql_psycopg2', 'postgresql', 'mysql', or 'oracle'.
-        'NAME': 'patchman',                    # Or path to database file if using sqlite3.
-        'USER': 'patchman',                    # Not used with sqlite3.
-        'PASSWORD': 'patchman',                # Not used with sqlite3.
-        'HOST': '',                            # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '',                            # Set to empty string for default. Not used with sqlite3.
-        'STORAGE_ENGINE': 'INNODB',
-        'CHARSET': 'utf8'
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': '/var/lib/patchman/db/patchman.db',
     }
 }
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
-# although not all choices may be available on all operating systems.
-# On Unix systems, a value of None will cause Django to use the same
-# timezone as the operating system.
-# If running in a Windows environment this must be set to the same as your
-# system time zone.
 TIME_ZONE = 'America/New_York'
 
 # Language code for this installation. All choices can be found here:
@@ -52,9 +41,9 @@ RUN_GUNICORN = False
 #STATICFILES_DIRS = ('/srv/patchman/media/',)
 
 # Enable memcached
-#CACHES = {
-#    'default': {
-#        'BACKEND': 'django.core.cache.backends.memcached.MemcachedCache',
-#        'LOCATION': '127.0.0.1:11211',
-#    }
-#}
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.memcached.MemcachedCache',
+        'LOCATION': '127.0.0.1:11211',
+    }
+}
