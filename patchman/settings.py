16e48d364a7ebd5f7331964422b03b4ca7dd6ebb
Marcus Furlong <furlongm@gmail.com>
2018-11-08 12:54:17 -0500

    add DEFAULT_PAGINATION_CLASS to silence warning on newer django_rest_framework

--- patchman/settings.py
+++ patchman/settings.py
@@ -98,7 +98,8 @@ LOCAL_APPS = (
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': ('rest_framework.permissions.IsAdminUser',),
-    'PAGE_SIZE': 10
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 100,
 }
 
 BROKER_HOST = 'localhost'
