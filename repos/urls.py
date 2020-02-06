07bb0a23c4f24d69b49f2ff557a16e022080b91e
Marcus Furlong <furlongm@gmail.com>
2020-02-06 23:58:16 +0000

    migrate to django.urls.path instead of django.conf.urls.url

--- repos/urls.py
+++ repos/urls.py
@@ -17,27 +17,21 @@
 
 from __future__ import unicode_literals
 
-from django.conf.urls import url
+from django.urls import path
 
 from repos import views
 
 app_name = 'repos'
 
 urlpatterns = [
-    url(r'^$', views.repo_list, name='repo_list'),
-    url(r'^(?P<repo_id>[-.\w]+)/delete/$', views.repo_delete,
-        name='repo_delete'),
-    url(r'^(?P<repo_id>[-.\w]+)/toggle_enabled/$', views.repo_toggle_enabled,
-        name='repo_toggle_enabled'),
-    url(r'^(?P<repo_id>[-.\w]+)/toggle_security/$', views.repo_toggle_security,
-        name='repo_toggle_security'),
-    url(r'^(?P<repo_id>[-.\w]+)/edit/$', views.repo_edit, name='repo_edit'),
-    url(r'^mirrors/$', views.mirror_list, name='mirror_list'),
-    url(r'^(?P<repo_id>[-.\w]+)/$', views.repo_detail, name='repo_detail'),
-    url(r'^mirrors/mirror/(?P<mirror_id>[-.\w]+)/$',
-        views.mirror_detail, name='mirror_detail'),
-    url(r'^mirrors/mirror/(?P<mirror_id>[-.\w]+)/delete/$',
-        views.mirror_delete, name='mirror_delete'),
-    url(r'^mirrors/mirror/(?P<mirror_id>[-.\w]+)/edit/$',
-        views.mirror_edit, name='mirror_edit'),
+    path('', views.repo_list, name='repo_list'),
+    path('<int:repo_id>/', views.repo_detail, name='repo_detail'),
+    path('<int:repo_id>/toggle_enabled/', views.repo_toggle_enabled, name='repo_toggle_enabled'),  # noqa
+    path('<int:repo_id>/toggle_security/', views.repo_toggle_security, name='repo_toggle_security'),  # noqa
+    path('<int:repo_id>/edit/', views.repo_edit, name='repo_edit'),
+    path('<int:repo_id>/delete/', views.repo_delete, name='repo_delete'),
+    path('mirrors/', views.mirror_list, name='mirror_list'),
+    path('mirrors/mirror/<int:mirror_id>/', views.mirror_detail, name='mirror_detail'),  # noqa
+    path('mirrors/mirror/<int:mirror_id>/edit/', views.mirror_edit, name='mirror_edit'),  # noqa
+    path('mirrors/mirror/<int:mirror_id>/delete/', views.mirror_delete, name='mirror_delete'),  # noqa
 ]
