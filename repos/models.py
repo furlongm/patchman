91c7a5760e23cfb398c2356cfabba0532ab228cc
Marcus Furlong <furlongm@gmail.com>
2020-07-09 19:19:19 -0400

    arch linux client support (#219)

--- repos/models.py
+++ repos/models.py
@@ -25,7 +25,7 @@ from arch.models import MachineArchitecture
 from packages.models import Package
 
 from repos.utils import refresh_deb_repo, refresh_rpm_repo, \
-    update_mirror_packages
+    refresh_arch_repo, update_mirror_packages
 from patchman.signals import info_message, warning_message, error_message
 
 
@@ -34,10 +34,12 @@ class Repository(models.Model):
 
     RPM = 'R'
     DEB = 'D'
+    ARCH = 'A'
 
     REPO_TYPES = (
         (RPM, 'rpm'),
         (DEB, 'deb'),
+        (ARCH, 'arch'),
     )
 
     name = models.CharField(max_length=255, unique=True)
@@ -86,6 +88,8 @@ class Repository(models.Model):
                 refresh_deb_repo(self)
             elif self.repotype == Repository.RPM:
                 refresh_rpm_repo(self)
+            elif self.repotype == Repository.ARCH:
+                refresh_arch_repo(self)
             else:
                 text = 'Error: unknown repo type for repo '
                 text += '{0!s}: {1!s}'.format(self.id, self.repotype)
