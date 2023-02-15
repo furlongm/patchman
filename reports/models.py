a84ea8a04fb62eaa227133dffc321c84f27d5e47
Marcus Furlong <furlongm@gmail.com>
2023-02-14 21:39:48 -0500

    allow blank for report_ip

--- reports/models.py
+++ reports/models.py
@@ -37,7 +37,7 @@ class Report(models.Model):
     kernel = models.CharField(max_length=255, null=True)
     arch = models.CharField(max_length=255, null=True)
     os = models.CharField(max_length=255, null=True)
-    report_ip = models.GenericIPAddressField(null=True)
+    report_ip = models.GenericIPAddressField(null=True, blank=True)
     protocol = models.CharField(max_length=255, null=True)
     useragent = models.CharField(max_length=255, null=True)
     processed = models.BooleanField(default=False)
