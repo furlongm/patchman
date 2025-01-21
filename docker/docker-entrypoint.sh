8f895511f20bea98c7a66da168c787601fef1d2d
Ricardo Jerónimo <rmjeronimo@netcabo.pt>
2025-01-21 12:18:31 +0000

    Entrypoint only starts Celery worker if an environment varible exists

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -4,7 +4,17 @@
 /usr/sbin/apache2ctl -DFOREGROUND &
 
 # Starts Celery for for realtime processing of reports from clients
-C_FORCE_ROOT=1 celery -b redis://redis:6379/0 -A patchman worker -l INFO -E
+if [ ! -z "${CELERY_BROKER}" ]; then
+    broker="${CELERY_BROKER}"
+
+    if [ ! -z "${CELERY_BROKER_PORT}" ]; then
+        brokerPort="${CELERY_BROKER_PORT}"
+    else
+        brokerPort=6379
+    fi
+
+    C_FORCE_ROOT=1 celery -b redis://"$broker":"$brokerPort"/0 -A patchman worker -l INFO -E
+fi
 
 # Wait for any process to exit
 wait -n
