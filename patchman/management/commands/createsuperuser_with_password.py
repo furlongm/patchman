f5b34d7e544f2c16160bd755e3406b30a0b1f3f9
Marcus Furlong <furlongm@gmail.com>
2025-10-20 21:15:08 -0400

    Update patchman/management/commands/createsuperuser_with_password.py
    Co-authored-by: code-review-doctor[bot] <72320148+code-review-doctor[bot]@users.noreply.github.com>

--- patchman/management/commands/createsuperuser_with_password.py
+++ patchman/management/commands/createsuperuser_with_password.py
@@ -6,7 +6,7 @@ class Command(createsuperuser.Command):
     help = 'Crate a superuser, and allow password to be provided'
 
     def add_arguments(self, parser):
-        super(Command, self).add_arguments(parser)
+        super().add_arguments(parser)
         parser.add_argument(
             '--password', dest='password', default=None,
             help='Specifies the password for the superuser.',
