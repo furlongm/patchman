d7c239c82dd88b2843a044409ffae95e05c9ea8b
Marcus Furlong <furlongm@gmail.com>
2026-03-04 22:26:10 -0500

    use pysqlite3 if available

--- patchman/settings.py
+++ patchman/settings.py
@@ -4,6 +4,13 @@ import os
 import site
 import sys
 
+# use pysqlite3 if available
+try:
+    import pysqlite3  # noqa
+    sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+except ImportError:
+    pass
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
